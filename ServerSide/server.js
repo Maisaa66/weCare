19180083b0acb27966fd38b52d3ba7c86dd8f31a
Nourhan Ahmed AboElenen <36276898+nourhanKg@users.noreply.github.com>
2023-04-15 22:34:24 +0200

    Setting Up Server & Database

---
+++ ServerSide/server.js
@@ -0,0 +1,18 @@
+//Server set app
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+const DBURL = process.env.DATABASE_URL.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DBURL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB connected successfully"));
+app.listen(PORT, () => console.log("localhost:" + PORT));
