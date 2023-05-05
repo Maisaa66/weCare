8a0081669cf91c807c820205c531ed732686411b
yaraElGazar <yara_elgazar@outlook.com>
2023-05-05 21:18:22 +0300

    add admin private route

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -8,12 +8,10 @@ import StepTwo from "./Pages/Signup/Step2";
 import StepThree from "./Pages/Signup/Step3";
 import StepFour from "./Pages/Signup/Step4";
 import StepFive from "./Pages/Signup/Step5";
-import PrivateRoute from "./Pages/PrivateRoute";
 import Filter from "./Pages/Filter/Filter";
 import UserDashBoard from "./Pages/userDashBoard/userDashBoard";
 import UserProfile from "./Pages/userDashBoard/userProfile";
 import ProviderProfile from "./Pages/providerDashBoard/providerProfile";
-import NavBar from "./components/Layout/NavBar/NavBar";
 import * as React from "react";
 import AdmindashBoard from "./components/AdminDashBoard/AdmindashBoard";
 import Users from "./components/AdminDashBoard/Main/Users/Users";
@@ -22,12 +20,11 @@ import Provider from "./Pages/Provider/Provider";
 import AllDetails from "./components/AdminDashBoard/Main/AllDetails/AllDetails";
 import Update from "./components/AdminDashBoard/Main/Update/Update";
 import ReqDetails from "./components/AdminDashBoard/Main/AllRequests/ReqDetails";
-import RequestTest from "./Pages/requestTest";
 import Requests from "./components/AdminDashBoard/Main/AllRequests/Requests";
 import ProviderDashBoard from "./Pages/providerDashBoard/providerDashBoard";
 import Charts from "./components/AdminDashBoard/Main/Charts/Charts";
 import CheckoutSuccess from "./components/UI/Checkout/CheckoutSuccess/CheckoutSuccess";
-// const authenticated = useSelector((state) => state.user.isAdmin);
+
 const router = createBrowserRouter([
   {
     path: "/admin",
@@ -107,10 +104,6 @@ const router = createBrowserRouter([
     element: <ProviderProfile />,
   },
   {
-    path: "/private",
-    element: <RequestTest />,
-  },
-  {
     path: "/checkout-success",
     element: <CheckoutSuccess />,
   },
@@ -120,23 +113,8 @@ function App() {
   return (
     <div className="App">
       <RouterProvider router={router}>
-        {/* <PrivateRoute
-          path="/userDashBoard"
-          component={UserDashBoard}
-          authenticated={authenticated}
-        /> */}
-
         <Outlet />
       </RouterProvider>
-      {/* <Home></Home> */}
-      {/* <FileUpload></FileUpload> */}
-      {/* <Login></Login> */}
-      {/* <Signup></Signup> */}
-      {/* <StepOne></StepOne> */}
-      {/* <StepThree></StepThree> */}
-      {/* <StepTwo></StepTwo> */}
-      {/* <Footer></Footer> */}
-      {/* <TextField id="filled-basic" label="Filled" variant="filled" /> */}
     </div>
   );
 }
--- clientside/src/components/AdminDashBoard/AdmindashBoard.js
+++ clientside/src/components/AdminDashBoard/AdmindashBoard.js
@@ -8,24 +8,35 @@ import * as ReactDOM from "react-dom";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import AllDetails from "./Main/AllDetails/AllDetails";
 import Update from "./Main/Update/Update";
+import { useSelector } from "react-redux";
+import { Route, Redirect, useNavigate, Navigate } from "react-router-dom";
+
 function AdmindashBoard() {
+  const isAuthenticated = useSelector((state) => state.user.isAdmin);
   return (
-    <div className={`${classes.box} d-flex px-5 overflow-hidden align-items-center`}>
-      
-      <div className={`shadow-lg bg-white rounded-5  ${classes.dashboard}`}>
-        <Nav></Nav>
-        <div className="row container-fluid py-4">
-          {/* <div className={`col-lg-2  col-6 ${classes.hit}`}>
+    <>
+      {isAuthenticated ? (
+        <div
+          className={`${classes.box} d-flex px-5 overflow-hidden align-items-center`}
+        >
+          <div className={`shadow-lg bg-white rounded-5  ${classes.dashboard}`}>
+            <Nav></Nav>
+            <div className="row container-fluid py-4">
+              {/* <div className={`col-lg-2  col-6 ${classes.hit}`}>
             <Aside></Aside>
           </div> */}
-          <div className="col-12">
-            <Outlet/>
-            {/* <Update/> */}
-            {/* <AllDetails></AllDetails> */}
+              <div className="col-12">
+                <Outlet />
+                {/* <Update/> */}
+                {/* <AllDetails></AllDetails> */}
+              </div>
             </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <Navigate to="/" replace />
+      )}
+    </>
   );
 }
 
